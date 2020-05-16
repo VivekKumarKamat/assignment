a749740318b5aca6a6c95cec7764afb1f3508903
VivekKumarKamat <65460358+VivekKumarKamat@users.noreply.github.com>
2020-05-16 22:36:51 +0530

    Update Leapyear.java

--- Leapyear.java
+++ Leapyear.java
@@ -1,34 +1,36 @@
 
+import java.util.Scanner;
 public class Leap 
 {
-
-	public static void main(String[] args)
-	{
-		 int year = 2019;
-	        boolean leap = false;
-
-	        if(year % 4 == 0)
-	        {
-	            if( year % 100 == 0)
-	            {
-	                if ( year % 400 == 0)
-	                    leap = true;
-	                else
-	                    leap = false;
-	            }
-	            else
-	                leap = true;
-	        }
-	        else
-	            leap = false;
-
-	        if(leap)
-	            System.out.println(year + " is a leap year.");
-	        else
-	            System.out.println(year + " is not a leap year.");
-	    }
-	
-
-	}
-
-
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter any year:");
+        int year = s.nextInt();
+        boolean flag = false;
+        if(year % 400 == 0)
+        {
+            flag = true;
+        }
+        else if (year % 100 == 0)
+        {
+            flag = false;
+        }
+        else if(year % 4 == 0)
+        {
+            flag = true;
+        }
+        else
+        {
+            flag = false;
+        }
+        if(flag)
+        {
+            System.out.println("Year "+year+" is a Leap Year");
+        }
+        else
+        {
+            System.out.println("Year "+year+" is not a Leap Year");
+        }
+    }
+}
