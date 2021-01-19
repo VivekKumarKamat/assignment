e8793a25e98282405b5b1e43865328a204c01acc
VivekKumarKamat <65460358+VivekKumarKamat@users.noreply.github.com>
2021-01-19 11:07:01 +0530

    Add files via upload

---
+++ Parrten/src/Star4.java
@@ -0,0 +1,18 @@
+
+public class Star4 {
+
+	
+	    public static void printStars(int n) 
+	    {
+	    	 int i,j,k;
+	    	for(i=1; i<=4; i++)
+	    	{
+	    	for(j=1; j<=4; j++)
+	    	{
+	    	System.out.print("*");
+	    	}
+	    	System.out.println(" ");
+	    	}
+	}
+
+}
